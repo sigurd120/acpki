07a2769ef708baa404db5b3e4d184aa6c14d7eaf
Sigurd Haaheim <sigurd.haaheim@gmail.com>
2019-03-21 15:25:54 +0100

    Add method for unrevoking a certificate.

--- acpki/pki/ocsp.py
+++ acpki/pki/ocsp.py
@@ -26,16 +26,30 @@ class OCSPResponder:
         try:
             revoked_file = open(self.revoked_file_path, "a")
             revoked_file.write(serial_number + '\n')
+            revoked_file.close()
         except IOError as e:
-            print("Could not revoke certificate: " + e)
+            print("Could not revoke certificate: {0}".format(e))
         else:
             print("Certificate \"{0}\" was revoked successfully.".format(serial_number))
-        finally:
-            revoked_file.close()
 
     def unrevoke_certificate(self, serial_number):
-        # TODO: Implement this method
-        pass
+        found = False
+        try:
+            with open(self.revoked_file_path, "r+") as f:
+                lines = f.readlines()
+                f.seek(0)
+                for line in lines:
+                    if line.strip() != serial_number:
+                        f.write(line)
+                    else:
+                        found = True
+                f.truncate()
+        except IOError as e:
+            print("Error: Could not unrevoke certificate: {0}".format(e))
+
+        if found:
+            print("Successfully unrevoked the certificate \"{0}\"".format(serial_number))
+        return found
 
     def is_revoked(self, serial_number):
         revoked_file = open(self.revoked_file_path, "r")
@@ -52,6 +66,7 @@ if __name__ == "__main__":
     ocsp_responder.revoke_certificate("fdsadfsaksks")
     ocsp_responder.revoke_certificate("dsaklklsdkdj")
     ocsp_responder.revoke_certificate("blablablabfd")
+    ocsp_responder.unrevoke_certificate("blablablabla")
     if ocsp_responder.is_revoked("blablablabla"):
         print("Certificate is revoked.")
     else:
